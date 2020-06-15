43fdb618b48b666a665a11fa767e9981f1f89a05
Alina <alina@yandex.ru>
2020-06-15 13:19:17 +0300

    Добавил redux в проект и настроил его

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
+const composeEnhancers =
+	typeof window === 'object' &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+			// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+		}) : compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(
+		applyMiddleware(
+			thunk
+		)
+	)
+)
+
+const app = (
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root')
 )
