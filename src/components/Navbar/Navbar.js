ddbc64fe435e5b0daf59f4820d7aba9084bb8e48
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-15 23:54:26 +0300

    Рефактор Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import classes from './Navbar.css'
+import { Link } from 'react-router-dom'
+import Input from '../UI/Input/Input'
+import classes from './Navbar.module.css'
 
 export const Navbar = () => {
 	return (
 		<div className={classes.Navbar}>
-
+			<Link to={'/'} className={classes.logo}>
+				<i className="fas fa-hamburger" />
+				&nbsp;
+				<span>MaxFood</span>
+			</Link>
+			<div className={classes.address}>
+				<i className="fas fa-map-marker-alt" />
+				<Input
+					type={'text'}
+					placeholder={'Введите адрес'}
+				/>
+			</div>
+			<Link to={'/auth'} className={classes.profile}>
+				<i className="fas fa-user" />
+				<span>Войти</span>
+			</Link>
+			<Link to={'/cart'} className={classes.cart}>
+				<i className="fas fa-shopping-cart" />
+				<span>Корзина</span>
+			</Link>
 		</div>
 	)
 }
