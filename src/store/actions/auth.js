790fab049d837f0473f83d3e5a67a016f337d8a0
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-16 11:21:26 +0300

    Добавил авторизацию

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -15,7 +15,7 @@ export default function auth(email, password, isLogin) {
 		const response = await axios.post(url, authData)
 		const data = response.data
 
-		const expirationDate = new Date(new Date().getTime + data.expiresIn * 1000)
+		const expirationDate = new Date((new Date().getTime() + data.expiresIn) * 1000)
 
 		localStorage.setItem('token', data.idToken)
 		localStorage.setItem('userId', data.localId)
@@ -29,6 +29,22 @@ export function authSuccess(token) {
 	return {
 		type: AUTH_SUCCESS,
 		token
+	}
+}
+export function autoLogin(params) {
+	return dispatch => {
+		const token = localStorage.getItem('token')
+		if (!token) {
+			dispatch(logout())
+		} else {
+			const expirationDate = new Date(localStorage.getItem('expirationDate'))
+			if (expirationDate <= new Date()) {
+				dispatch(logout())
+			} else {
+				dispatch(authSuccess(token))
+				dispatch(autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000))
+			}
+		}
 	}
 }
 export function autoLogout(time) {
