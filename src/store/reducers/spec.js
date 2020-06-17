6f59fabfdae6191d660d98b1bf460b52ef262790
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-17 18:21:32 +0300

    Сделал кейсы для экшенов

---
+++ src/store/reducers/spec.js
@@ -0,0 +1,30 @@
+import {FETCH_SPECS_ERROR, FETCH_SPECS_START, FETCH_SPECS_SUCCESS} from '../actions/actionTypes'
+
+const initialState = {
+	specs: [],
+	loading: true,
+	erorr: null
+}
+export default function specReducer(state = initialState, action) {
+	switch (action.type) {
+		case FETCH_SPECS_START:
+			return {
+				...state,
+				loading: true
+			}
+		case FETCH_SPECS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				specs: action.specs
+			}
+		case FETCH_SPECS_ERROR:
+			return {
+				...state,
+				loading: false,
+				specs: action.error
+			}
+		default:
+			return state
+	}
+}
